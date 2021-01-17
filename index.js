cbf742c91cbe7974d57502e2aa0d81f1392ec2bc
Shelator222 <77592418+Shelator222@users.noreply.github.com>
2021-01-17 20:06:13 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+
+function empezar() {
+
+
+$("#titulo").css({
+
+"font-family": "Hachi Maru Pop",
+"font-size": 60,
+"color": "red",
+"display": "flex",
+"align-self": "center",
+"width": "90vw",
+"justify-content": "center",
+"height": "20vh",
+"padding-top": "50px",
+"padding-left": "70px"
+
+});
+
+
+// Titulo con JQuery
+
+$("#titulo").html("La pagina de Vani");
+
+// Titulo con JavaScript de los 90s
+
+// document.getElementById("titulo").innerHTML = "choto"
+
+// holaaaaaaa
+
+$("#parrafo").css({
+    "font-family": "Pacifico",
+    "font-size": 30,
+    "color": "blue",
+    "display": "flex",
+    "align-self": "center",
+    "width": "90vw",
+    "justify-content": "center",
+    "height": "30vh",
+    "padding-top": "40px",
+    "padding-left": "70px"
+    });
+
+    $("#parrafo").html("Hola. Me llamo Vani y soy programadora. Contrataciones al 0800-666-6666");
+
+}
